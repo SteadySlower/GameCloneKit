732c8f8ae2fa7df66021cc9d81472f3773e1299e
JW Moon <moon910904@gmail.com>
2024-08-17 20:56:23 +0900

    chore: remove test target

--- PuzzleKit/Package.swift
+++ PuzzleKit/Package.swift
@@ -15,12 +15,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PuzzleKit"
-        ),
-        .testTarget(
-            name: "PuzzleKitTests",
-            dependencies: ["PuzzleKit"],
+            name: "PuzzleKit",
             path: "Sources/PuzzleKit"
-        ),
+        )
     ]
 )
