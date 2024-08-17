f84ed290f3826ae05d341b0222e418644e0eeb51
JW Moon <moon910904@gmail.com>
2024-08-17 20:55:42 +0900

    chore: add App

---
+++ GameCloneApp/GameCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCloneAppApp.swift
+//  GameCloneApp
+//
+//  Created by JW Moon on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GameCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
