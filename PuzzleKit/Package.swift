ac55adcedc3d0fd156b50fe70d22eaa495173bc1
JW Moon <moon910904@gmail.com>
2024-08-17 20:52:56 +0900

    chore: add path

--- PuzzleKit/Package.swift
+++ PuzzleKit/Package.swift
@@ -15,9 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PuzzleKit"),
+            name: "PuzzleKit"
+        ),
         .testTarget(
             name: "PuzzleKitTests",
-            dependencies: ["PuzzleKit"]),
+            dependencies: ["PuzzleKit"],
+            path: "Sources/PuzzleKit"
+        ),
     ]
 )
